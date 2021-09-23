b9ddfd85fbfe20351db6d1c81ff3579874118078
Diego Castro Garcés <invasivewand69@gmail.com>
2021-09-23 00:02:50 -0300

    Auto stash before merge of "Diego" and "master"

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -10,13 +11,18 @@ export class HomePage {
 
   nombreUsuario='';
 
-  constructor(private activeroute:ActivatedRoute,private router:Router) {
+  constructor(private activeroute: ActivatedRoute,private router: Router, private menu: MenuController) {
     this.activeroute.queryParams.subscribe(params => {
       if (this.router.getCurrentNavigation().extras.state) {
         this.nombreUsuario = this.router.getCurrentNavigation().extras.state.miusuario.username;
         console.log(this.nombreUsuario);
       }
 });
+}
+
+openMenuEnd() {
+  console.log(this.openMenuEnd);
+  this.menu.open('custom');
 }
 
 }
