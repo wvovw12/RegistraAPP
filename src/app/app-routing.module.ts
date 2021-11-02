c6213958f18dc217a1c79a63e350273031780cc3
4Absolem <Moisexys@gmail.com>
2021-11-02 17:42:59 -0300

    cambios en pg. clases

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes, CanActivate } from '@angular/router';
-import { LoginGuardGuard } from './guards/login-guard.guard';
-
-const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule),
-    canActivate:[LoginGuardGuard]
-  },
-  {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'clases',
-    loadChildren: () => import('./pages/clases/clases.module').then( m => m.ClasesPageModule)
-  },
-  {
-    path: 'passrecover',
-    loadChildren: () => import('./pages/passrecover/passrecover.module').then( m => m.PassrecoverPageModule)
-  },
-  {
-    path: 'registro',
-    loadChildren: () => import('./pages/registro/registro.module').then( m => m.RegistroPageModule)
-  },
-  {
-    path: 'escanear',
-    loadChildren: () => import('./pages/escanear/escanear.module').then( m => m.EscanearPageModule)
-  },
-
-
-];
-
-@NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
-=======
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes, CanActivate } from '@angular/router';
 import { LoginGuardGuard } from './guards/login.guard';
@@ -79,6 +31,10 @@ const routes: Routes = [
     path: 'registro',
     loadChildren: () => import('./pages/registro/registro.module').then( m => m.RegistroPageModule)
   },
+  {
+    path: 'escanear',
+    loadChildren: () => import('./pages/escanear/escanear.module').then( m => m.EscanearPageModule)
+  },
 
 
 ];
@@ -90,4 +46,3 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
->>>>>>> Diego
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,6 +41,10 @@ export class AppComponent {
   }
 
 
+
+  
+
+
 }
 
 
