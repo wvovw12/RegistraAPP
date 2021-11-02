2587e10f94e4dcfa3e219965a082029f4a5f4123
Diego Castro Garcés <invasivewand69@gmail.com>
2021-11-02 18:06:04 -0300

    +

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAKH6b9btPCI4kbccxUy-3lxj5jpCstM58',
+    authDomain: 'registraapp-ebc02.firebaseapp.com',
+    projectId: 'registraapp-ebc02',
+    storageBucket: 'registraapp-ebc02.appspot.com',
+    messagingSenderId: '183711011975',
+    appId: '1:183711011975:web:5f42472e31c05fa4d81422',
+    measurementId: 'G-3YC3NH687H'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAKH6b9btPCI4kbccxUy-3lxj5jpCstM58',
+    authDomain: 'registraapp-ebc02.firebaseapp.com',
+    projectId: 'registraapp-ebc02',
+    storageBucket: 'registraapp-ebc02.appspot.com',
+    messagingSenderId: '183711011975',
+    appId: '1:183711011975:web:5f42472e31c05fa4d81422',
+    measurementId: 'G-3YC3NH687H'
+  }
 };
 
 /*
